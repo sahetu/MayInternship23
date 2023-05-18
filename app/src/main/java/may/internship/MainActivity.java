e2b72b4082d337d2e53bc021b588d4d083cfdc23
Sagar Khatri <khatrisagar2@gmail.com>
2023-05-18 11:50:43 +0530

    17th May Code Push

--- app/src/main/java/may/internship/MainActivity.java
+++ app/src/main/java/may/internship/MainActivity.java
@@ -1,14 +1,35 @@
 package may.internship;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        login = findViewById(R.id.main_login);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("Login Successfully");
+                Log.d("LOGIN","Login Successfully");
+                Log.e("LOGIN","Login Successfully");
+
+                Toast.makeText(MainActivity.this,"Login Successfuly",Toast.LENGTH_LONG).show();
+
+            }
+        });
+
     }
 }
